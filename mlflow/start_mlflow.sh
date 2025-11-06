e0b46395b9395a215c638ed0e8e2932493ca378e
BolotnikovaAA <83873279+Vivaldition@users.noreply.github.com>
2025-11-06 16:38:28 +0300

    Update start_mlflow.sh

--- mlflow/start_mlflow.sh
+++ mlflow/start_mlflow.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-export MLFLOW_TRACKING_URI="sqlite:///$PWD/mlflow/mlruns.db"
-mlflow ui --backend-store-uri $MLFLOW_TRACKING_URI --port 5000 --host 127.0.0.1
+mlflow server --backend-store-uri sqlite:///mlruns.db
